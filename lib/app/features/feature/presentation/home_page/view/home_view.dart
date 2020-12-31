6dbdb42b97ae2623e6c3d21294ff765dd77bd21d
Omer Gamliel <omergamliel3@gmail.com>
2020-12-31 12:52:18 +0200

    add home view simple ui

--- lib/app/features/feature/presentation/home_page/view/home_view.dart
+++ lib/app/features/feature/presentation/home_page/view/home_view.dart
@@ -1,52 +1,66 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:flutter_boilerplate_project/injector.dart';
+
+import '../controller/cubit_controller.dart';
+import '../controller/state.dart';
+
+import '../../../../../core/widgets/loading_widget.dart';
+import '../widgets/widgets.dart';
+
 class HomeView extends StatelessWidget {
   PreferredSizeWidget appBar() {
-    return AppBar();
+    return AppBar(
+      title: const Text('HomePage'),
+    );
   }
 
   Widget buildBody(BuildContext context) {
-    return Container();
+    final textStyle =
+        Theme.of(context).textTheme.headline4.copyWith(color: Colors.white);
+
+    return BlocBuilder<HomeViewController, HomeState>(
+      builder: (context, state) {
+        return state.when(
+          initial: () => Center(
+              child: Text(
+            'Initial State',
+            style: textStyle,
+          )),
+          loading: () => LoadingWidget(),
+          success: (connection) => Center(
+            child: Text(
+              'Success State\nConnection: $connection',
+              style: textStyle,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          error: (failure) => Center(
+            child: Text(
+              'Failure state\nReason: ${failure.message}',
+              style: textStyle,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       top: false,
-      child: Scaffold(
-        backgroundColor: Colors.yellow,
-        appBar: appBar(),
-        body: buildBody(context),
+      child: BlocProvider(
+        create: (_) => Injector.resolve<HomeViewController>(),
+        child: Scaffold(
+          appBar: appBar(),
+          body: buildBody(context),
+          floatingActionButton: FAB(),
+        ),
       ),
     );
   }
 }
-
-// IMPLEMENTING BLOC, CUBIT EXAMPLE
-
-//   Widget buildBody(BuildContext context) {
-//   return BlocBuilder<ArticlesCubit, ArticlesState>(
-//     builder: (context, state) {
-//       return state.when(
-//         initial: () => core_widgets.LoadingWidget(),
-//         loading: () => core_widgets.LoadingWidget(),
-//         success: (articles) => ArticlesView(articles: articles),
-//         error: (failure) => core_widgets.ErrorWidget(failure.message),
-//       );
-//     },
-//   );
-// }
-// Widget build(BuildContext context) {
-//   return SafeArea(
-//     top: false,
-//     child: BlocProvider(
-//       create: (_) => Injector.resolve<ArticlesCubit>()..getArticles(),
-//       child: Scaffold(
-//         backgroundColor: Colors.yellow,
-//         appBar: appBar(),
-//         body: buildBody(context),
-//         floatingActionButton: FAB(),
-//       ),
-//     ),
-//   );
-// }
