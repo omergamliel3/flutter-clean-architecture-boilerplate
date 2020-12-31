1d690d5955db9bffd5ccbe2d6f7892d442074ffe
Omer Gamliel <omergamliel3@gmail.com>
2020-12-31 13:28:12 +0200

    setup theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_boilerplate_project/app/config/app_config.dart';
 
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'app/config/app_config.dart';
 import 'app/routes/app_routes.dart';
+
+import 'app/theme/theme_provider.dart';
+import 'app/theme/theme_state.dart';
+import 'app/theme/theme.dart';
 
 import 'injector.dart';
 
@@ -17,14 +23,25 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: AppConfig.APP_NAME,
-      theme: ThemeData.dark(),
-      themeMode: ThemeMode.dark,
-      navigatorKey: Routes.sailor.navigatorKey,
-      initialRoute: Pages.SPLASH,
-      onGenerateRoute: Routes.sailor.generator(),
-    );
+    return BlocProvider(
+        create: (_) => ThemeProvider(),
+        lazy: false,
+        child: BlocBuilder<ThemeProvider, ThemeState>(
+          builder: (context, state) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: AppConfig.APP_NAME,
+              theme: lightThemeData(),
+              darkTheme: darkThemeData(),
+              themeMode: state.when(
+                  light: () => ThemeMode.light,
+                  dark: () => ThemeMode.dark,
+                  system: () => ThemeMode.system),
+              navigatorKey: Routes.sailor.navigatorKey,
+              initialRoute: Pages.SPLASH,
+              onGenerateRoute: Routes.sailor.generator(),
+            );
+          },
+        ));
   }
 }
