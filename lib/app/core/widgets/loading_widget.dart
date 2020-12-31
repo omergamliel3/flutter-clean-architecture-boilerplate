56fee3f0ef62a7517ca8184568bab493dc8a6b3e
Omer Gamliel <omergamliel3@gmail.com>
2020-12-31 12:50:25 +0200

    change value color to accent color

--- lib/app/core/widgets/loading_widget.dart
+++ lib/app/core/widgets/loading_widget.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 class LoadingWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    final color = Theme.of(context).accentColor;
+    return Center(
       child: CircularProgressIndicator(
-        valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
+        valueColor: AlwaysStoppedAnimation<Color>(color),
       ),
     );
   }
