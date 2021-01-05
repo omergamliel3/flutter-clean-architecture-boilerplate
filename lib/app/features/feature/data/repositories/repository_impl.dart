7422e76123644483fce4390fe63e8f892d650a45
Omer Gamliel <omergamliel3@gmail.com>
2021-01-05 16:23:19 +0200

    currect file name

--- lib/app/features/feature/data/repositories/repository_impl.dart
+++ lib/app/features/feature/data/repositories/repository_impl.dart
@@ -11,7 +11,7 @@
 
 import 'package:dartz/dartz.dart';
 import 'package:flutter_boilerplate_project/app/core/errors/failure.dart';
-import 'package:flutter_boilerplate_project/app/features/feature/domain/entities/entitiy.dart';
+import 'package:flutter_boilerplate_project/app/features/feature/domain/entities/entity.dart';
 import 'package:flutter_boilerplate_project/app/features/feature/domain/repositories/repository.dart';
 
 class EntitiesRepositoryImpl implements Repository {
